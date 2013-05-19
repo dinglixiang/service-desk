28ba3a379722223118a7cdc5d1a02c211d704f03
尹常鑫 <ycxxww@gmail.com>
2013-05-20 01:39:06 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceDesk::Application.config.secret_token = '66700cd12a90fb47b63fc3148d50bac0cec59fcbb25feafc00208732870f54ad93eee542e83416973f48d0f32b14368885136a03372f910db632d27cab4db203'
